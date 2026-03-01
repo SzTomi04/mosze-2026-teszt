bfa0fd07d456572da319afa1469f322781e1f219
SzTomi04 <szelenyitamas10@gmail.com>
2026-03-01 17:21:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;
+constexpr int N_ELEMENTS = 100; // N_ELEMENTS rossz néven van meghívva
 
 int main()
 {
     int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    std::cout << '1-100 ertekek duplazasa' 	// több karakter használata a '' aposztróffal és hiányzik a pontosvessző
+    for (int i = 0;) 				// hiányzik a ciklus feltétel és inkrementálás
     {
-        b[i] = i * 2;
+        b[i] = i * 2; 				// végtelen ciklus
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) 			// ciklusfeltétel hibás
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" 			// nem ír ki értéket és hiányzik a pontosvessző
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;					// változó nincs inicializálva
+    for (int i = 0; i < N_ELEMENTS, i++)	// vessző van pontosvessző helyett a feltételben
     {
-        atlag += b[i]
+        atlag += b[i]				// hiányzik a pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
