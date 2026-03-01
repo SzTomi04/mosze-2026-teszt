ccc439d1bdf7fd298c124d434fa81428ed1f5cb7
SzTomi04 <szelenyitamas10@gmail.com>
2026-03-01 17:35:39 +0100

    hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100; // N_ELEMENTS rossz néven van meghívva
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa' 	// több karakter használata a '' aposztróffal és hiányzik a pontosvessző
-    for (int i = 0;) 				// hiányzik a ciklus feltétel és inkrementálás
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;	// több karakter használata a '' aposztróffal és hiányzik a pontosvessző
+    for (int i = 0; i < N_ELEMENTS; i++)	// hiányzik a ciklus feltétel és inkrementálás
     {
-        b[i] = i * 2; 				// végtelen ciklus
+        b[i] = i * 2;	// végtelen ciklus
     }
-    for (int i = 0; i; i++) 			// ciklusfeltétel hibás
+    for (int i = 0; i < N_ELEMENTS; i++)	// ciklusfeltétel hibás
     {
-        std::cout << "Ertek:" 			// nem ír ki értéket és hiányzik a pontosvessző
+        std::cout << "Ertek: " << b[i] << std::endl;	// nem ír ki értéket és hiányzik a pontosvessző
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;					// változó nincs inicializálva
-    for (int i = 0; i < N_ELEMENTS, i++)	// vessző van pontosvessző helyett a feltételben
+    int atlag = 0;	// változó nincs inicializálva
+    for (int i = 0; i < N_ELEMENTS; i++)	// vessző van pontosvessző helyett a feltételben
     {
-        atlag += b[i]				// hiányzik a pontosvessző
+        atlag += b[i];	// hiányzik a pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    delete[] b;
     return 0;
 }
